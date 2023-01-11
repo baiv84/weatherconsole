9386f050e25e1b79457db923e856f7d18ef3d1a3
Default User U <u@uits-17-sm>
2023-01-11 13:36:17 +0700

    Iter 2 - Fix some bugs

---
+++ tools/customexception.py
@@ -0,0 +1,16 @@
+class ExceptionWeather(Exception):
+    """Define class for custom weather exception"""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        print('calling str')
+        if self.message:
+            return 'Weather download exception, {0} '.format(self.message)
+        else:
+            return 'ExceptionWeather has been raised'
